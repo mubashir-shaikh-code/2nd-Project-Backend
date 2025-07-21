c19fee64e0cd40dec3b377c1c0e15c5d7f21e751
mubashir-shaikh-code <mubashiraijaz1@gmail.com>
2025-07-21 13:09:16 +0500

    clean

--- Models/Models.js
+++ Models/Models.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+  const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  profilePic: { type: String }, // Optional: base64 or file path
-});
+  const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: String }, // Optional: base64 or file path
+  });
 
 
-module.exports = mongoose.model('User', userSchema);
+  module.exports = mongoose.model('User', userSchema);
