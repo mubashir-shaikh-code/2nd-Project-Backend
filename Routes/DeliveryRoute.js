09a6314ece207844d471ab2f8fcb6d963ad13e27
mubashir-shaikh-code <mubashiraijaz1@gmail.com>
2025-08-05 10:54:38 +0500

    cart

--- Routes/DeliveryRoute.js
+++ Routes/DeliveryRoute.js
@@ -5,15 +5,19 @@ const {
   getAllOrders,
   createCartOrders,
   updateOrderStatus,
+    getUserOrders,
   UserOrders 
 } = require('../Controllers/DeliveryController');
 const verifyToken = require('../Middleware/Auth');
+
+
 
 // 🛒 Place order (user)
 router.post('/place', verifyToken, createOrder);
 
 router.post('/create-cart-orders', verifyToken, createCartOrders);
 
+router.get('/orders', verifyToken, getUserOrders);
 // 👤 User: Get logged-in user's orders
 router.get('/my-orders', verifyToken, UserOrders);
 
